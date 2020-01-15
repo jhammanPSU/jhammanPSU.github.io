40735c8d66152619b2d4fac1f3834c4525617671
jhammanPSU <jhammanPSU@users.noreply.github.com>
2020-01-15 01:27:19 -0800

    Homework Updates
    Lots of Updates
    HWs & Homepage Edits/Uploads

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+alert("hi there");
+
+function showNav() {
+
+	$('#topnav').html(
+		`
+		<ul>
+			<li class="resources-link"><a href="index.html">Home</a></li>
+			<li><a href="about.html">About Josh</a></li>
+			<li><a href="skills.html">Skill Sets</a></li>
+			<li><a href="projects.html">Past Projects</a></li>
+		</ul>
+		`
+	);
+
+}
+
+function addActiveClass(){
+	if ( $('body').class('.favorite-foods') {
+		alert('Favorite Foods!');
+	} else if( $(){
+
+	} else {
+
+	}
+
+}
+
+
+
+function initialize(){
+	showNav();
+	addActiveClass();
+
+$(initialize());
+
+
+});
+// if() {
+// }
+
+
+
+// for() {
+// }
